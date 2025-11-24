2ab03a9306c571ade332d6a6fe584f71d88d9877
Abo_Tolba <122931774+AhmedElbassuony@users.noreply.github.com>
2025-11-24 10:52:25 +0200

    Create Content And Accounts Pages And Crreate Info Card Component And use it in home page

---
+++ apps/content/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config({ path: ".env.local" }); // 👈 حمل القيم من .env.local
+
+const { createServer } = require("http");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 4008;
+
+app.prepare().then(() => {
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}/content`);
+  });
+});
